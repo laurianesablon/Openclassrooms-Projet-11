a28e24483b0031c17cefcafca313de79ec877ad1
Lauriane <109591568+stressedoutdyk3@users.noreply.github.com>
2023-09-28 13:07:48 +0200

    appel api opé

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,14 @@
-import { setToken } from "../store/store";
+import { setToken, setUser } from "../store/store";
+import { apiURL } from "./apiURL";
 
-export const fetchLogin = async (url, email, password, setResponse, dispatch, navigate) => {
+export const fetchLogin = async (
+  email,
+  password,
+  setResponse,
+  dispatch,
+  navigate
+) => {
+  try {
     const data = { email, password };
     const options = {
       method: "POST",
@@ -9,49 +17,71 @@ export const fetchLogin = async (url, email, password, setResponse, dispatch, na
       },
       body: JSON.stringify(data),
     };
-    try {
-      const response = await fetch(url, options);
-      const responseData = await response.json();
-      setResponse(response); // Add this line
 
-      if (response.ok) {
-        let token = responseData.body.token;
-        dispatch(setToken(token));
-        await fetchUserData(url, dispatch);
+    const response = await fetch(apiURL + "/login", options);
+    const responseData = await response.json();
+    setResponse(response);
 
-        navigate("/profile");
-      } else {
-        console.log("error");
-      }
-      console.log(response);
-      console.log(email, password);
-    } catch (error) {
-      console.log(error);
+    if (response.ok) {
+      const token = responseData.body.token;
+      await fetchUserData(token, dispatch);
+      dispatch(setToken(token));
+      navigate("/profile");
+    } else {
+      throw new Error(responseData.message);
     }
-  };
+  } catch (Error) {
+    console.log(Error);
+  }
+};
 
-  export const fetchUserData = async (url, token, dispatch) => {
+export const fetchUserData = async (token, dispatch) => {
+  try {
+    const response = await fetch(apiURL + "/profile", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        accept: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({}),
+    });
+    console.log(response);
+
+    if (response.ok) {
+      const responseData = await response.json();
+      const { body } = responseData;
+      if (body) {
+        const { firstName, lastName, userName, id } = body;
+        const user = { firstName, lastName, userName, id };
+        dispatch(setUser(user));
+      } else {
+        throw new Error(responseData.message);
+      }
+    }
+  } catch (Error) {
+    console.log(Error);
+  }
+};
+
+export const changeUsername = async (newUsername, token, dispatch, firstName, lastName, id) => {
     try {
-      const response = await fetch(url, {
-        method: "POST",
+      const response = await fetch(`${apiURL}/profile`, {
+        method: "PUT",
         headers: {
           "Content-Type": "application/json",
           "accept": "application/json",
           "Authorization": `Bearer ${token}`,
         },
-        body: JSON.stringify({}),
+        body: JSON.stringify({ newUsername }),
       });
   
       if (response.ok) {
-        const responseData = await response.json();
-        if (responseData.body) {
-          const { firstName, lastName, userName, id } = responseData.body;
-          const user = { firstName, lastName, userName, id };
-          dispatch(setUser(user));
-        }
+        dispatch(setUser({ firstName, lastName, userName: newUsername, id }));
+      } else {
+        throw new Error(response.statusText);
       }
     } catch (error) {
-      console.error(error);
+      throw new Error(error);
     }
   };
-  
