faff897a8d790125e3310fedd563e36b3693b3fc
Lauriane <109591568+stressedoutdyk3@users.noreply.github.com>
2023-10-04 19:52:25 +0200

    modifs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,21 @@
-import Home from './pages/home';
-import Connexion from './pages/connexion';
-import User from './pages/user';
-import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+import Home from "./pages/home";
+import Connexion from "./pages/connexion";
+import User from "./pages/user";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./assets/styles/main.css";
 import "./assets/styles/login.css";
-import PrivateRoutes from './utils/privateRoutes';
-import { useSelector } from 'react-redux';
-import { useEffect } from 'react';
-
-
+import PrivateRoutes from "./utils/privateRoutes";
 
 function App() {
-  // const userId = useSelector((state) => state.user.id);
-  // console.log(userId)
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Connexion />} />
-            <Route element={<PrivateRoutes/>}>
-              <Route path={"/profile"} element={<User />} />
-            </Route>
+          <Route element={<PrivateRoutes />}>
+            <Route path={"/profile"} element={<User />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
