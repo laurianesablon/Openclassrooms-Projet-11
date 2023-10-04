3ee955e717d65c0addffe4268f4ae62cca8e760e
Lauriane <109591568+stressedoutdyk3@users.noreply.github.com>
2023-10-04 19:48:09 +0200

    ajouts transactions

--- frontend/src/components/account.js
+++ frontend/src/components/account.js
@@ -1,14 +1,56 @@
-function Account({accountTitle, accountAmount, accountAmountDescription}) {
-    return (
-        <section class="account">
-        <div class="account-content-wrapper">
-          <h3 class="account-title">Argent Bank Checking (x8349)</h3>
-          <p class="account-amount">$2,082.79</p>
-          <p class="account-amount-description">Available Balance</p>
+import Transactions from "../components/transactions";
+import { useState } from "react";
+function Account({ accountTitle, accountAmount, accountAmountDescription }) {
+  const [toggle, setToggle] = useState(false);
+  return (
+    <>
+      <section className="account">
+        <div className="account-content-wrapper">
+          <h3 className="account-title">{accountTitle}</h3>
+          <p className="account-amount">{accountAmount}</p>
+          <p className="account-amount-description">
+            {accountAmountDescription}
+          </p>
         </div>
-        <div class="account-content-wrapper cta">
-          <button class="transaction-button">View transactions</button>
+        <div className="account-content-wrapper cta">
+          <button className="transaction-button" onClick={() => setToggle(!toggle)}>View transactions</button>
         </div>
       </section>
-    )
+      <section className="transaction">
+        {toggle && (
+          <>
+            <div className="transactions_table_title">
+              <p>Date</p>
+              <p>Description</p>
+              <p>Amount</p>
+              <p>Balance</p>
+            </div>
+
+            <Transactions
+              date="27/02/2021"
+              description="Golden Sun Bakery"
+              amount="$8.00"
+              balance="$298.00"
+              transactionType="Electronic"
+              category="Food"
+              note="lorem ipsum"
+            />
+            <Transactions
+              date="27/02/2021"
+              description="Golden Sun Bakery"
+              amount="$8.00"
+              balance="$298.00"
+              transactionType="Electronic"
+              category="Food"
+              note="lorem ipsum"
+            />
+      
+          
+          </>
+        )}
+      </section>
+    </>
+  );
 }
+
+export default Account;
