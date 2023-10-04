9d23cf02846dba5ad1ef59c53bfbf2e2a1954317
Lauriane <109591568+stressedoutdyk3@users.noreply.github.com>
2023-10-04 19:46:59 +0200

    ajout transactions

---
+++ frontend/src/components/transactions.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+export default function Transactions({
+  date,
+  description,
+  amount,
+  balance,
+  transactionType,
+  category,
+  note,
+}) {
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <div className="transactions_table">
+      <div className="c1">
+        <p>{date}</p>
+        {toggle && (
+          <>
+            <p>Transaction type</p>
+            <p>Category</p>
+            <p>Note</p>
+          </>
+        )}
+      </div>
+      <div className="c2">
+        <p>{description}</p>
+        {toggle &&
+        <>
+          <p>{transactionType}</p>
+          <p>{category} <i class="fa-solid fa-pencil"></i></p>
+          <p>{note} <i class="fa-solid fa-pencil"></i></p>
+        </>
+        }
+        
+      </div>
+      <div className="c3">
+        <p>{amount}</p>
+      </div>
+      <div className="c4">
+        <p>{balance}</p>
+      </div>
+      <i className="fa-solid fa-chevron-down white" onClick={() => setToggle(!toggle)}></i>
+    </div>
+  );
+}
