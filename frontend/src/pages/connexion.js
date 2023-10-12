91530dbe75e3720f3cb53f9d24f24d57196dfae9
Lauriane <109591568+stressedoutdyk3@users.noreply.github.com>
2023-10-12 13:31:31 +0200

    refactor fonction

--- frontend/src/pages/connexion.js
+++ frontend/src/pages/connexion.js
@@ -4,52 +4,37 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchLogin } from "../utils/api";
 import { ErrorMessage } from "../components/error_message";
+import FormFields from "../components/fields";
 
 function Connexion() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  });
   const [response, setResponse] = useState("");
   const message = useSelector((state) => state.errorMessage.message);
-  console.log(useSelector((state) => state.errorMessage))
-
   const handleFetchData = async (event) => {
     event.preventDefault();
-    await fetchLogin(email, password, setResponse, dispatch, navigate, message); // Call the fetchData function
+    await fetchLogin(formData.email, formData.password, setResponse, dispatch, navigate, message); // Call the fetchData function
+  };
+  const handleFieldChange = (name, value) => {
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
   };
 
   return (
-    <Layout>
+    <Layout onlySignIn={true}>
       <main className="main bg-dark">
         <section className="sign-in-content">
           <i className="fa fa-user-circle sign-in-icon"></i>
           <h1>Sign In</h1>
           <form onSubmit={handleFetchData}>
-            <div className="input-wrapper">
-              <label htmlFor="email">Email</label>
-              <input
-                type="text"
-                id="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className={
-                  !response.ok && response ? "shake message_erreur_label" : ""
-                }
-              />
-            </div>
-            <div className="input-wrapper">
-              <label htmlFor="password">Password</label>
-              <input
-                type="password"
-                id="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className={
-                  !response.ok && response ? "shake message_erreur_label" : ""
-                }
-              />
-            </div>
+            <FormFields name="email" label="Email" value={formData.email} onChange={handleFieldChange} type="text"/>
+            <FormFields name="password" label="Password" value={formData.password} onChange={handleFieldChange} type="password"/>
             {!response?.ok && response && (
               <div className="message_erreur">
                 Incorrect username or password
